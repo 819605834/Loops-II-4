22ca079449c97db7006d2796bb2f67083f01a1f6
819605834 <124411012+819605834@users.noreply.github.com>
2026-06-04 21:43:25 +0000

    Created findCumulativeSum

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // Write your code below
+var num = 5;
+
+function findCumulativeSum(num){
+    let sum = 0;
+    if (num != 0) {
+        for (let i = 1;i<=num;i++){
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+console.log(findCumulativeSum(num));
